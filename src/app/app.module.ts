5d90cbf0b44a6b745c78dfcd114a1b775c935693
Marek Pacześny <mpaczes@tlen.pl>
2025-04-24 16:27:58 +0200

    adding module MatButtonModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatButtonModule,
     HttpClientModule
   ],
   providers: [],
