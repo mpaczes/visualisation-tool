8c173442214ee3351ecc3dc5325624afe1bf129e
Marek Pacześny <mpaczes@tlen.pl>
2025-04-24 10:10:40 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vis-tool-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'visualisation-tool';
+}
